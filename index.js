084cc96240b2a8a8f37478c1de4b9a0eee7714ce
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-02 02:13:36 -0800

    pass tests

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var queuer  = require('level-queue')
+var queue   = require('level-queue')
 var hooks   = require('level-hooks')
 var uuid    = require('node-uuid')
 var iterate = require('iterate')
@@ -7,7 +7,7 @@ var iterate = require('iterate')
 function between(key, range) {
   return (
     (!range.start || range.start <= key) &&
-    (!range.end || key <= job.end)
+    (!range.end || key <= range.end)
   )
 }
 
@@ -19,30 +19,33 @@ module.exports = function (db) {
 
   db.trigger = {
     add: function (range, job) {
-
       if(!job) job = range.job
       else     range.job = job
 
       range.name = range.name || uuid()
+      range.start = ''
+      range.end = '~'
       ranges[range.name] = range
-      range.map = range.map || function (e) { return e }
-      db.queue.add('queue:'+range.name, job)
+      range.map = range.map || function (e) { return e.key }
+      db.queue.add('trigger:'+range.name, job)
     }
   }
 
   queue()(db)
   hooks()(db)
 
-  hooks.pre(function (batch) {
+  db.hooks.pre(function (batch) {
     var insert = []
-
     //for each operation inside each range,
     //atomically add a record, or run a the job.
     iterate.join(batch, ranges, function (item, range, _, name) {
-      var key = ''+item.key
-      if(between(key, range))
-        insert.push(db.queue(job.name, job.map(key), false))
+      var key = ''+item.key, mapped
+      if(between(key, range) && (mapped = range.map(item))) {
+
+        insert.push(db.queue('trigger:' + range.name, mapped, false))
+      }
     })
+
     return batch.concat(insert)
   })
 }
